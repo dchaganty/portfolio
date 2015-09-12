bd8246e317fe22ff5f2eef92c974aadf260f7c76
dchaganty <dhc5cu@virginia.edu>
2015-09-12 14:11:59 -0400

    Update Stopwatch.java
    Keeps track of the amount of time the player spends trying to kill the ghosts.

--- Android-game/Stopwatch.java
+++ Android-game/Stopwatch.java
@@ -25,10 +25,10 @@ public class Stopwatch extends Thread
     {
         long milliTime = System.currentTimeMillis() - this.startTime;
         int[] out = new int[]{0, 0, 0, 0};
-        out[0] = (int)(milliTime / 3600000      );
-        out[1] = (int)(milliTime / 60000        ) % 60;
-        out[2] = (int)(milliTime / 1000         ) % 60;
-        out[3] = (int)(milliTime)                 % 1000;
+        out[0] = (int)(milliTime / 3600000);
+        out[1] = (int)(milliTime / 60000) % 60;
+        out[2] = (int)(milliTime / 1000) % 60;
+        out[3] = (int)(milliTime) % 1000;
 
         return out;
     }
