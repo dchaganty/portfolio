a915b93d00bb5a4632a0c79ffc6660afd55b1fbb
dchaganty <dhc5cu@virginia.edu>
2015-09-12 13:56:26 -0400

    Update Hunter.java
    This file creates the Hunter class.  The hunter is controlled by the user and is used to kill the ghosts in the game.

--- Android-game/Hunter.java
+++ Android-game/Hunter.java
@@ -6,18 +6,17 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Hunter {
-	private float xPos;
-	private float yPos;
+    private float xPos;'
+    private float yPos;
     private GameView gameView;
     private Bitmap character;
    
     public Hunter(GameView gameView, Bitmap char1) {
           this.gameView=gameView;
           this.character=char1;
-          
     }
 
-   
+   //Draws the hunter image to the screen
     public void onDraw(Canvas canvas, float hx, float hy) {
     	this.xPos = hx;
     	this.yPos = hy;
@@ -25,10 +24,12 @@ public class Hunter {
 
     }
     
+    //gets the x coordinate of the hunter's position
     public float getXPos() {
     	return xPos;
     }
     
+    //gets the y coordinate of the hunter's position
     public float getYPos() {
     	return yPos;
     }
